8c5891595eba72df9063191aabafd42aa93e1df4
Luís Felipe <luisfelipe.assuncao@hotmail.com>
2022-04-23 21:59:38 -0400

    feat: added swagger ui

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import swagger from "swagger-ui-express";
 
+import config from "../swagger.json";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
@@ -7,5 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+app.use("/api-docs", swagger.serve, swagger.setup(config));
 
 export { app };
